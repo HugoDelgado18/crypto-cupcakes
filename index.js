50293e555739e13d0f806d1fb0fc4621dd7a31c9
hugo <hugo.delgado@clover.com>
2023-02-23 12:23:02 -0500

    Auth0 is completed

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
+const { auth } = require('express-openid-connect');
+const {
+  AUTH0_SECRET,
+  AUTH0_AUDIENCE, 
+  AUTH0_CLIENT_ID,
+  AUTH0_BASE_URL,
+} = process.env;
+
+const config = {
+  authRequired: true, // this is different from the documentation
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_AUDIENCE,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: AUTH0_BASE_URL,
+};
+
+app.use(auth(config));
 
 const { User, Cupcake } = require('./db');
 
@@ -20,6 +38,10 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+})
+
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
